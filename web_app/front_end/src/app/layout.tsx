78ceaa9a352a8315b88b97d976f854a85a9f7384
Oleg Zapara <ole9zp@gmail.com>
2025-10-04 09:51:10 +0300

    react query

--- web_app/front_end/src/app/layout.tsx
+++ web_app/front_end/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "@/lib/providers/Providers";
 
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased dark px-3 md:px-6 sm:px-4 py-1`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
